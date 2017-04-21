ab8e3e7f3143cbbbc11da45242bea0b84fe3df49
Jason E <jason.espada@van-l-mm111-63.vancouver.wsu.edu>
2017-04-21 15:23:18 -0700

    Level Loading and Prefabs

---
+++ Assets/EndLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player")
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("Scene2");
+	}
+}
