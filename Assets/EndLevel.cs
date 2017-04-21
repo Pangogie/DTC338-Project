444a8c32fd55c6909df9e6de7fd77df01d8092a4
Jason E <jason.espada@van-l-mm111-63.vancouver.wsu.edu>
2017-04-21 15:39:11 -0700

    Chesty Changes

--- Assets/EndLevel.cs
+++ Assets/EndLevel.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class EndLevel : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
+		StartCoroutine(HandleEnd(other));
+	}
+
+	IEnumerator HandleEnd(Collider other)
+	{
+		yield return new WaitForSeconds(3);
+
 		if (other.tag == "Player")
 			UnityEngine.SceneManagement.SceneManager.LoadScene ("Scene2");
 	}
