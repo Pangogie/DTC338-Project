83c9de8bf84616ae167e12129a421d52f32988e4
Jason E <pangogie@gmail.com>
2017-05-01 22:00:57 -0700

    Ty Learned To Run

--- Assets/Scripts/PlayerModel.cs
+++ Assets/Scripts/PlayerModel.cs
@@ -6,17 +6,22 @@ public class PlayerModel : MonoBehaviour {
     public int health = 10;
     public int attack;
     public int chests = 0;
+    private TyController ty;
 
 
 	// Use this for initialization
 	void Start () {
-		
+        ty = GetComponent<TyController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (health <= 0)
             Die();
+        if (Input.GetKey(KeyCode.LeftShift))
+            ty.speed = 8;
+        else
+            ty.speed = 4;
 	}
 
     void OnCollisionEnter(Collision other)
@@ -38,4 +43,5 @@ public class PlayerModel : MonoBehaviour {
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
+
 }
